0dab0cca8f92be7d252cf87f17758a48073f99f4
energetiq <energetiq@outlook.com>
2024-05-09 00:52:21 +0200

    first commit

---
+++ includes/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sae";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+?>
---
+++ includes/nav.php
@@ -0,0 +1,14 @@
+<div class="v1_2">
+        <div class="v1_8">
+            <div class="v1_3"></div>
+            <div class="v1_4"></div>
+            <div class="v1_19"></div>
+            <a href="index.php">
+                <img class="v1_20" src="./images/house-solid.svg">
+            </a>
+            <div class="v1_6">
+                <a href="register.php" class="v1_6_1">Register</a>
+            </div>
+            <div class="v1_7"></div>
+        </div>
+    </div>
