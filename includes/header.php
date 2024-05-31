165cfcc581a8e08e1d3d2f8b20817fedb2b47e12
Energetiq <46401261+NergYR@users.noreply.github.com>
2024-05-31 15:35:51 +0000

    ..

--- includes/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'includes/nav.php';
-
-?>
--- index.php
+++ index.php
@@ -1,19 +1,61 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <link href="./css/main.css" rel="stylesheet" />
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <title>Home</title>
-</head>
-
-<body>
-    <?php include 'includes/header.php'; ?>
-    
-</body>
-
-</html>
+<div class="Accueil" style="width: 1440px; height: 1024px; position: relative; background: white">
+  <div class="Navback" style="width: 1440px; height: 87px; left: 0px; top: 0px; position: absolute">
+    <div class="Rectangle1" style="width: 1440px; height: 87px; left: 0px; top: 0px; position: absolute; background: #D9D9D9"></div>
+    <div class="Rectangle2" style="width: 356px; height: 87px; left: 1084px; top: 0px; position: absolute; background: #A9A5A5; border-top-left-radius: 90px"></div>
+    <div class="Rectangle3" style="width: 356px; height: 87px; left: 0px; top: 0px; position: absolute; background: #A9A5A5; border-top-left-radius: 90px"></div>
+    <div class="Rectangle3" style="width: 116px; height: 35px; left: 1157px; top: 26px; position: absolute; background: #D9D9D9; border-radius: 15px"></div>
+    <div class="Rectangle4" style="width: 116px; height: 35px; left: 1288px; top: 26px; position: absolute; background: #D9D9D9; border-radius: 15px"></div>
+  </div>
+  <div class="Other" style="width: 616px; height: 390px; left: 759px; top: 556px; position: absolute">
+    <div class="Rectangle10" style="width: 616px; height: 390px; left: 0px; top: 0px; position: absolute; background: #D9D9D9"></div>
+    <div class="Ellipse3" style="width: 317px; height: 134px; left: 51px; top: 34px; position: absolute; background: #B3B3B3; border-radius: 9999px"></div>
+    <div class="Ellipse4" style="width: 317px; height: 134px; left: 51px; top: 228px; position: absolute; background: #B3B3B3; border-radius: 9999px"></div>
+    <div class="Image2" style="width: 152px; height: 148px; left: 427px; top: 221px; position: absolute">
+      <div class="Rectangle12" style="width: 152px; height: 148px; left: 0px; top: 0px; position: absolute; background: #B3B3B3"></div>
+      <div class="Line8" style="width: 212.15px; height: 0px; left: 0px; top: 0px; position: absolute; transform: rotate(44.24deg); transform-origin: 0 0; border: 1px black solid"></div>
+      <div class="Line9" style="width: 212.15px; height: 0px; left: 152px; top: 0px; position: absolute; transform: rotate(135.76deg); transform-origin: 0 0; border: 1px black solid"></div>
+    </div>
+    <div class="Image2" style="width: 152px; height: 148px; left: 427px; top: 20px; position: absolute">
+      <div class="Rectangle12" style="width: 152px; height: 148px; left: 0px; top: 0px; position: absolute; background: #B3B3B3"></div>
+      <div class="Line8" style="width: 212.15px; height: 0px; left: 0px; top: 0px; position: absolute; transform: rotate(44.24deg); transform-origin: 0 0; border: 1px black solid"></div>
+      <div class="Line9" style="width: 212.15px; height: 0px; left: 152px; top: 0px; position: absolute; transform: rotate(135.76deg); transform-origin: 0 0; border: 1px black solid"></div>
+    </div>
+  </div>
+  <div class="Caroussel" style="width: 1310px; height: 298px; left: 65px; top: 169px; position: absolute">
+    <div class="Rectangle7" style="width: 1310px; height: 298px; left: 0px; top: 0px; position: absolute; background: #D9D9D9"></div>
+    <div class="Image" style="width: 292px; height: 195px; left: 97px; top: 54px; position: absolute">
+      <div class="Rectangle11" style="width: 292px; height: 195px; left: 0px; top: 0px; position: absolute; background: #B3B3B3"></div>
+      <div class="Line6" style="width: 350.02px; height: 0px; left: 0px; top: 0px; position: absolute; transform: rotate(33.46deg); transform-origin: 0 0; border: 1px black solid"></div>
+      <div class="Line7" style="width: 350.02px; height: 0px; left: 292px; top: 0px; position: absolute; transform: rotate(146.54deg); transform-origin: 0 0; border: 1px black solid"></div>
+    </div>
+    <div class="Image" style="width: 292px; height: 197px; left: 514px; top: 54px; position: absolute">
+      <div class="Rectangle11" style="width: 292px; height: 195px; left: 0px; top: 2px; position: absolute; background: #B3B3B3"></div>
+      <div class="Line6" style="width: 350.02px; height: 0px; left: 0px; top: 0px; position: absolute; transform: rotate(33.46deg); transform-origin: 0 0; border: 1px black solid"></div>
+      <div class="Line7" style="width: 350.02px; height: 0px; left: 292px; top: 0px; position: absolute; transform: rotate(146.54deg); transform-origin: 0 0; border: 1px black solid"></div>
+    </div>
+    <div class="Rectangle11" style="width: 292px; height: 193px; left: 931px; top: 52px; position: absolute; background: #B3B3B3"></div>
+    <div class="Image" style="width: 292px; height: 191.02px; left: 931px; top: 52px; position: absolute">
+      <div class="Line6" style="width: 348.93px; height: 0px; left: 0px; top: 0px; position: absolute; transform: rotate(33.19deg); transform-origin: 0 0; border: 1px black solid"></div>
+      <div class="Line7" style="width: 348.93px; height: 0px; left: 292px; top: 0px; position: absolute; transform: rotate(146.81deg); transform-origin: 0 0; border: 1px black solid"></div>
+    </div>
+  </div>
+  <div class="Most" style="width: 616px; height: 390px; left: 71px; top: 556px; position: absolute">
+    <div class="Rectangle8" style="width: 616px; height: 390px; left: 0px; top: 0px; position: absolute; background: #D9D9D9"></div>
+    <div class="Ellipse1" style="width: 317px; height: 134px; left: 248px; top: 49px; position: absolute; background: #B3B3B3"></div>
+    <div class="Ellipse2" style="width: 317px; height: 134px; left: 248px; top: 243px; position: absolute; background: #B3B3B3; border-radius: 9999px"></div>
+    <div class="Image2" style="width: 152px; height: 148px; left: 31px; top: 35px; position: absolute">
+      <div class="Rectangle12" style="width: 152px; height: 148px; left: 0px; top: 0px; position: absolute; background: #B3B3B3"></div>
+      <div class="Line8" style="width: 212.15px; height: 0px; left: 0px; top: 0px; position: absolute; transform: rotate(44.24deg); transform-origin: 0 0; border: 1px black solid"></div>
+      <div class="Line9" style="width: 212.15px; height: 0px; left: 152px; top: 0px; position: absolute; transform: rotate(135.76deg); transform-origin: 0 0; border: 1px black solid"></div>
+    </div>
+    <div class="Image2" style="width: 152px; height: 148px; left: 31px; top: 229px; position: absolute">
+      <div class="Rectangle12" style="width: 152px; height: 148px; left: 0px; top: 0px; position: absolute; background: #B3B3B3"></div>
+      <div class="Line8" style="width: 212.15px; height: 0px; left: 0px; top: 0px; position: absolute; transform: rotate(44.24deg); transform-origin: 0 0; border: 1px black solid"></div>
+      <div class="Line9" style="width: 212.15px; height: 0px; left: 152px; top: 0px; position: absolute; transform: rotate(135.76deg); transform-origin: 0 0; border: 1px black solid"></div>
+    </div>
+  </div>
+  <div class="Promo1" style="width: 199px; height: 65px; left: 454px; top: 653px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">Promo 1</div>
+  <div class="BestSell1" style="width: 199px; height: 65px; left: 943px; top: 653px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">Best Sell 1</div>
+  <div class="BestSell1" style="width: 199px; height: 65px; left: 943px; top: 859px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">Best Sell 1</div>
+  <div class="Promo1" style="width: 199px; height: 65px; left: 454px; top: 859px; position: absolute; color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">Promo 1</div>
+</div>
