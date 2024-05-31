e09bdbdb9caccaa0f6ac0df8a43a2d140f922aea
Energetiq <46401261+NergYR@users.noreply.github.com>
2024-05-31 16:14:57 +0000

    .

--- index.php
+++ index.php
@@ -1,71 +1,70 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Layout</title>
-    <link rel="stylesheet" href="css/main.css">
+  <title>Accueil</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-    <div class="Accueil">
-        <div class="Navback">
-            <div class="Rectangle1"></div>
-            <div class="Rectangle2"></div>
-            <div class="Rectangle3"></div>
-            <div class="Rectangle4"></div>
-        </div>
-        <div class="Other">
-            <div class="Rectangle10"></div>
-            <div class="Ellipse3"></div>
-            <div class="Ellipse4"></div>
-            <div class="Image2">
-                <div class="Rectangle12"></div>
-                <div class="Line8"></div>
-                <div class="Line9"></div>
-            </div>
-            <div class="Image2">
-                <div class="Rectangle12"></div>
-                <div class="Line8"></div>
-                <div class="Line9"></div>
-            </div>
-        </div>
-        <div class="Caroussel">
-            <div class="Rectangle7"></div>
-            <div class="Image">
-                <div class="Rectangle11"></div>
-                <div class="Line6"></div>
-                <div class="Line7"></div>
-            </div>
-            <div class="Image">
-                <div class="Rectangle11"></div>
-                <div class="Line6"></div>
-                <div class="Line7"></div>
-            </div>
-            <div class="Rectangle11"></div>
-            <div class="Image">
-                <div class="Line6"></div>
-                <div class="Line7"></div>
-            </div>
-        </div>
-        <div class="Most">
-            <div class="Rectangle8"></div>
-            <div class="Ellipse1"></div>
-            <div class="Ellipse2"></div>
-            <div class="Image2">
-                <div class="Rectangle12"></div>
-                <div class="Line8"></div>
-                <div class="Line9"></div>
-            </div>
-            <div class="Image2">
-                <div class="Rectangle12"></div>
-                <div class="Line8"></div>
-                <div class="Line9"></div>
-            </div>
-        </div>
-        <div class="Promo1">Promo 1</div>
-        <div class="BestSell1">Best Sell 1</div>
-        <div class="BestSell1">Best Sell 1</div>
-        <div class="Promo1">Promo 1</div>
+  <div class="Accueil">
+    <div class="Navback">
+      <div class="Rectangle1"></div>
+      <div class="Rectangle2"></div>
+      <div class="Rectangle3"></div>
+      <div class="Rectangle3"></div>
+      <div class="Rectangle4"></div>
     </div>
+    <div class="Other">
+      <div class="Rectangle10"></div>
+      <div class="Ellipse3"></div>
+      <div class="Ellipse4"></div>
+      <div class="Image2" style="left: 427px; top: 221px;">
+        <div class="Rectangle12"></div>
+        <div class="Line8"></div>
+        <div class="Line9"></div>
+      </div>
+      <div class="Image2" style="left: 427px; top: 20px;">
+        <div class="Rectangle12"></div>
+        <div class="Line8"></div>
+        <div class="Line9"></div>
+      </div>
+    </div>
+    <div class="Caroussel">
+      <div class="Rectangle7"></div>
+      <div class="Image">
+        <div class="Rectangle11"></div>
+        <div class="Line6"></div>
+        <div class="Line7"></div>
+      </div>
+      <div class="Image">
+        <div class="Rectangle11"></div>
+        <div class="Line6"></div>
+        <div class="Line7"></div>
+      </div>
+      <div class="Rectangle11" style="left: 931px; top: 52px;"></div>
+      <div class="Image" style="left: 931px; top: 52px;">
+        <div class="Line6"></div>
+        <div class="Line7"></div>
+      </div>
+    </div>
+    <div class="Most">
+      <div class="Rectangle8"></div>
+      <div class="Ellipse1"></div>
+      <div class="Ellipse2"></div>
+      <div class="Image2" style="left: 31px; top: 35px;">
+        <div class="Rectangle12"></div>
+        <div class="Line8"></div>
+        <div class="Line9"></div>
+      </div>
+      <div class="Image2" style="left: 31px; top: 229px;">
+        <div class="Rectangle12"></div>
+        <div class="Line8"></div>
+        <div class="Line9"></div>
+      </div>
+    </div>
+    <div class="Promo1" style="left: 454px; top: 653px;">Promo 1</div>
+    <div class="BestSell1" style="left: 943px; top: 653px;">Best Sell 1</div>
+    <div class="BestSell1" style="left: 943px; top: 859px;">Best Sell 1</div>
+    <div class="Promo1" style="left: 454px; top: 859px;">Promo 1</div>
+  </div>
 </body>
 </html>
