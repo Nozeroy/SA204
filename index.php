348c8401d9fd5525d52cd9a80b077c754cc9ec98
Energetiq <46401261+NergYR@users.noreply.github.com>
2024-05-31 16:05:16 +0000

    .

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-    <link rel="stylesheet" href="css/main.css">
+    <title>Page Layout</title>
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
     <div class="Accueil">
         <div class="Navback">
             <div class="Rectangle1"></div>
             <div class="Rectangle2"></div>
-            <div class="Rectangle3 rectangle3-1"></div>
-            <div class="Rectangle3 rectangle3-2"></div>
+            <div class="Rectangle3"></div>
             <div class="Rectangle4"></div>
         </div>
         <div class="Other">
             <div class="Rectangle10"></div>
             <div class="Ellipse3"></div>
             <div class="Ellipse4"></div>
-            <div class="Image2 image2-1">
+            <div class="Image2">
                 <div class="Rectangle12"></div>
                 <div class="Line8"></div>
                 <div class="Line9"></div>
             </div>
-            <div class="Image2 image2-2">
+            <div class="Image2">
                 <div class="Rectangle12"></div>
                 <div class="Line8"></div>
                 <div class="Line9"></div>
@@ -32,18 +31,18 @@
         </div>
         <div class="Caroussel">
             <div class="Rectangle7"></div>
-            <div class="Image image-1">
+            <div class="Image">
                 <div class="Rectangle11"></div>
                 <div class="Line6"></div>
                 <div class="Line7"></div>
             </div>
-            <div class="Image image-2">
+            <div class="Image">
                 <div class="Rectangle11"></div>
                 <div class="Line6"></div>
                 <div class="Line7"></div>
             </div>
-            <div class="Rectangle11 rectangle11-1"></div>
-            <div class="Image image-3">
+            <div class="Rectangle11"></div>
+            <div class="Image">
                 <div class="Line6"></div>
                 <div class="Line7"></div>
             </div>
@@ -52,21 +51,21 @@
             <div class="Rectangle8"></div>
             <div class="Ellipse1"></div>
             <div class="Ellipse2"></div>
-            <div class="Image2 image2-3">
+            <div class="Image2">
                 <div class="Rectangle12"></div>
                 <div class="Line8"></div>
                 <div class="Line9"></div>
             </div>
-            <div class="Image2 image2-4">
+            <div class="Image2">
                 <div class="Rectangle12"></div>
                 <div class="Line8"></div>
                 <div class="Line9"></div>
             </div>
         </div>
         <div class="Promo1">Promo 1</div>
-        <div class="BestSell1 bestsell1-1">Best Sell 1</div>
-        <div class="BestSell1 bestsell1-2">Best Sell 1</div>
-        <div class="Promo1 promo1-2">Promo 1</div>
+        <div class="BestSell1">Best Sell 1</div>
+        <div class="BestSell1">Best Sell 1</div>
+        <div class="Promo1">Promo 1</div>
     </div>
 </body>
 </html>
